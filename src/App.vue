6d61412fa1b8945a1fead35a40de4ae95b8001ea
justkimkime <justkimkime@gmail.com>
2023-06-30 10:55:12 +0900

    vue-todo 기본

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <ToDoHeader></ToDoHeader>
+    <ToDoInput></ToDoInput>
+    <ToDoList></ToDoList>
+    <ToDoFooter></ToDoFooter>
+  </div>
+</template>
+
+<script>
+
+import ToDoHeader from './components/ToDoHeader.vue';
+import ToDoInput from './components/ToDoInput.vue';
+import ToDoList from './components/ToDoList.vue';
+import ToDoFooter from './components/ToDoFooter.vue';
+
+
+export default {
+  components:{
+    'ToDoHeader':ToDoHeader,
+    'ToDoInput':ToDoInput,
+    'ToDoList':ToDoList,
+    'ToDoFooter':ToDoFooter,
+  }
+}
+</script>
+
+<style>
+
+</style>
