e624f31733eaf148d8c20e53fa7e1703ea2bf943
justkimkime <justkimkime@gmail.com>
2023-07-02 21:54:34 +0900

    투두 기본기능구현

--- src/App.vue
+++ src/App.vue
@@ -26,5 +26,8 @@ export default {
 </script>
 
 <style>
-
+body{text-align: center; background-color:#f6f6f6;}
+input{border-style: none; width:calc(100% - 3rem);float:left;box-sizing: border-box; }
+button{border-style: groove;}
+.shadow{box-shadow:5px 10px 10px rgba(0,0,0,0.03)}
 </style>
--- src/components/ToDoInput.vue
+++ src/components/ToDoInput.vue
@@ -1,13 +1,35 @@
 <template>
-  <div>input</div>
+  <div class="inputBox shadow">
+    <input type="text" v-model="newTodoItem" v-on:keyup.enter="addToDo"/>
+    <!-- <button v-on:click="addToDo">add</button> -->
+    <span class="addContainer" v-on:click="addToDo"><i class="fas fa-plus addBtn"></i></span>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data:function(){
+    return {
+      newTodoItem:''
+    }
+  },
+  methods:{
+    addToDo:function(){
+      //localStorage.setItem(키 값, 밸류 값); 
+      localStorage.setItem(this.newTodoItem, this.newTodoItem);
+      this.clearInput();
+    },
+    clearInput:function(){
+      this.newTodoItem = '';
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+input:focus{outline: none;}
+.inputBox{background:#fff; height: 50px;line-height: 50px;border-radius: 5px;}
+.inputBox input{border-style: none; font-size: 0.9rem;height: 50px;line-height: 50px;}
+.addContainer{float: right;background:linear-gradient(to right, #6478fb,#8763fb);display: block;width: 3rem;border-radius: 0 5px 5px 0;}
+.addBtn{color:#fff; vertical-align: middle;}
 </style>
